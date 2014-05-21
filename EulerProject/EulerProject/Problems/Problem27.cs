d918947361e0344085887182af3799700294da0f
Christian Kaczmarek <kaczmar2@outlook.com>
2014-05-21 15:37:33 +0000

    tweak problem 27 code.  Enable code analysis
    git-tfs-id: [https://kaczmar2.visualstudio.com/DefaultCollection]$/Euler Project;C15

--- EulerProject/EulerProject/Problems/Problem27.cs
+++ EulerProject/EulerProject/Problems/Problem27.cs
@@ -10,12 +10,11 @@ namespace EulerProject.Problems
     {
         public object Solve()
         {
-            var product = 0;
             const int min = -1000;
             const int max = 1000;
             int largestCount = 0;
-            int c1 = min; 
-            int c2 = min;
+            int a1 = min; 
+            int a2 = min;
             for (int a = min; a <= max; a++)
             {
                 for (int b = min; b <= max; b++)
@@ -38,14 +37,14 @@ namespace EulerProject.Problems
                     if (count > largestCount)
                     {
                         largestCount = count;
-                        c1 = a;
-                        c2 = b;
-                        Debug.WriteLine("a={0} b={1}: {2}", c1, c2, count);
+                        a1 = a;
+                        a2 = b;
+                        //Debug.WriteLine("a={0} b={1}: {2}", a1, a2, count);
                     }
                 }
             }
-            product = c1*c2;
-            return product;
+            //Debug.WriteLine("a={0} b={1}: {2}", a1, a2, largestCount);
+            return a1 * a2;
         }
     }
 }
