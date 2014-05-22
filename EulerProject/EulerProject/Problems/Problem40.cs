60acce0d947b1bb9a11586c23ac4f71be576c72c
Christian Kaczmarek <kaczmar2@outlook.com>
2014-05-22 21:25:22 +0000

    Solved problem 40
    git-tfs-id: [https://kaczmar2.visualstudio.com/DefaultCollection]$/Euler Project;C22

---
+++ EulerProject/EulerProject/Problems/Problem40.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace EulerProject.Problems
+{
+    class Problem40 : IProblem
+    {
+        /// <summary>
+        /// http://projecteuler.net/problem=40
+        /// </summary>
+        public object Solve()
+        {
+            const int max = 1000000;
+            var d = new StringBuilder("."); // shift the index over one for readability
+            for (int i = 1; i < max; i++)
+            {
+                d.Append(i);
+            }
+
+            var res = 
+                int.Parse(d[1].ToString(CultureInfo.InvariantCulture)) * 
+                int.Parse(d[10].ToString(CultureInfo.InvariantCulture)) * 
+                int.Parse(d[100].ToString(CultureInfo.InvariantCulture)) * 
+                int.Parse(d[1000].ToString(CultureInfo.InvariantCulture)) *
+                int.Parse(d[10000].ToString(CultureInfo.InvariantCulture)) * 
+                int.Parse(d[100000].ToString(CultureInfo.InvariantCulture)) * 
+                int.Parse(d[1000000].ToString(CultureInfo.InvariantCulture));
+            return res;
+        }
+    }
+}
