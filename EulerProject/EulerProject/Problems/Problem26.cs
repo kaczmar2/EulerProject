c9ad65d6bc58e898db4a77920751b05cc51dbb40
Christian Kaczmarek <kaczmar2@outlook.com>
2014-05-30 20:21:32 +0000

    Solved problem 26
    git-tfs-id: [https://kaczmar2.visualstudio.com/DefaultCollection]$/Euler Project;C36

---
+++ EulerProject/EulerProject/Problems/Problem26.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EulerProject.Problems
+{
+    /// <summary>
+    /// http://projecteuler.net/problem=26
+    /// http://en.wikipedia.org/wiki/Cyclic_number
+    /// Originally this was solved in 3 guesses: from above article, knowing that the digital period of 1/p (where p is prime) is p − 1,
+    /// We know repeat cycles are longer the closer d is to 999.  So I tried the three largest primes less than 1000: 997, 991, 983.  983 is correct.
+    /// This simple formula yields failed cases (997, 991).  Implementation of correct way (long divsion test) is below.
+    /// </summary>
+    class Problem26 : IProblem
+    {
+        public object Solve()
+        {
+            const int max = 1000;
+            const int b = 10;   // Base 10
+            var primes = new List<int>();
+            int result = 0;
+            
+            // collect all primes < 1000
+            for (int p = max; p > 0; p--)
+            {
+                if (Common.IsPrime(p) && b % p != 0)
+                {
+                    // prime that does not divide b
+                    primes.Add(p);
+                }
+            }
+
+            foreach (var prime in primes)
+            {
+                int t = 0;
+                double r = 1;
+                double n = 0;
+                do
+                {
+                    double x = r * b;
+                    double d = Math.Floor(x / prime);
+                    t++;
+                    r = x % prime;
+                    n = n * b + d;
+                } while (!r.Equals(1));
+
+                if (t == prime - 1)
+                {
+                    result = prime;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
--- EulerProject/EulerProject/Program.cs
+++ EulerProject/EulerProject/Program.cs
@@ -9,7 +9,7 @@ namespace EulerProject
         static void Main(string[] args)
         {
             // SOLVE A SINGLE PROBLEM
-            const int pNumber = 33;
+            const int pNumber = 26;
             var sw = new Stopwatch();
             string pFullName = string.Concat("EulerProject.Problems.Problem", pNumber.ToString("D2"));
             var pDisplayName = pFullName.Substring(pFullName.LastIndexOf('.') + 1, pFullName.Length - 1 - pFullName.LastIndexOf('.'));
