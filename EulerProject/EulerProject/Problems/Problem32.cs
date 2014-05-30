9c770c481117f7fe02a4a6418f1fb5313c23985f
Christian Kaczmarek <kaczmar2@outlook.com>
2014-05-30 23:20:48 +0000

    Solved problem 32
    git-tfs-id: [https://kaczmar2.visualstudio.com/DefaultCollection]$/Euler Project;C38

---
+++ EulerProject/EulerProject/Problems/Problem32.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace EulerProject.Problems
+{
+    /// <summary>
+    /// http://projecteuler.net/problem=32
+    /// If multiplier (b) has 4 digits, product has to have at least 4 digits, so just do some very rough loop limiting
+    /// Still brute froce and slow (50 secs).
+    /// </summary>
+    class Problem32 : IProblem
+    {
+        public object Solve()
+        {
+            var products = new List<int>();
+            const int min = 1;
+            const int max = 9876;
+
+            for (int a = min; a <= max; a++)
+            {
+                for (int b = min; b <= max; b++)
+                {
+                    var c = a*b;
+                    string m = a.ToString(CultureInfo.InvariantCulture);
+                    string n = b.ToString(CultureInfo.InvariantCulture);
+                    string p = c.ToString(CultureInfo.InvariantCulture);
+                    string pd = string.Concat(m, n, p);
+                    if (pd.Length == 9 && Common.IsPandigital(int.Parse(pd)))
+                    {
+                        products.Add(c);
+                    }
+                }
+            }
+            return products.Distinct().ToList().Sum();
+        }
+    }
+}
